dec8883cffc8aefd6270500ddfb1c9fad7a6c5fa
Hermansyah <herman.piliang23@gmail.com>
2022-09-05 12:54:36 +0700

    add third test case

---
+++ tests/modules/3. problem_three.part.js
@@ -0,0 +1,15 @@
+const { sumData } = require("../../src/math_problem");
+const { expect } = require("chai");
+
+const problem_three_test = () => {
+  it("has to be 10", () => {
+    const result = sumData(2, 2, process.env.STATIC_NUMBER);
+    expect(result).eql(10);
+  });
+};
+
+if (process.env.TEST_MODE !== "batch") {
+  describe("problem three", problem_three_test.bind(this));
+}
+
+module.exports = problem_three_test;
