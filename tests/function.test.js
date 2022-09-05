57e896ba32506f58dab9d68e24d19df633e6ac18
Hermansyah <herman.piliang23@gmail.com>
2022-09-05 12:55:59 +0700

    add third test case to function test

--- tests/function.test.js
+++ tests/function.test.js
@@ -3,8 +3,10 @@ process.env.TEST_MODE = "batch";
 
 const problemOne = require("./modules/1. problem_one.part");
 const problemTwo = require("./modules/2. problem_two.part")(4);
+const problemThree = require("./modules/3. problem_three.part");
 
 describe("math problem", () => {
   describe("problem one", problemOne.bind(this));
   describe("problem two", problemTwo.bind(this));
+  describe("problem three", problemThree.bind(this));
 });
